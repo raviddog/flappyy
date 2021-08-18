01f3c8833ea7a6e530b4982df1bb8be4c20884d4
raviddog <raviddog@outlook.com>
2021-08-19 02:22:36 +1000

    e

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,26 @@
 #include "engine/engine.hpp"
 
-const int scrWidth = 640;
-const int scrHeight = 480;
+extern void load_game();
+extern void logic_game();
+extern void draw_game();
+extern void unload_game();
 
 
 int main(int argv, char** args) {
     // engine::init("Flappy Aya", "./flappyy.ini");
     engine::init("Flappy Aya", 0, true, 640, 480);
 
+    load_game();
+
     while(!engine::quit) {
         engine::inputs();
 
+        logic_game();
+        draw_game();
         
         engine::flip();
     }
+    unload_game();
     engine::close();
     return 0;
 }
