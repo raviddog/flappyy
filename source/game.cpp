59548b08297375deb8059efc267b11761ccafde6
raviddog <raviddog@outlook.com>
2021-09-07 00:50:20 +1000

    background pillars

--- source/game.cpp
+++ source/game.cpp
@@ -1,14 +1,15 @@
 #include "engine/engine.hpp"
-#include <queue>
+#include <vector>
+#include <ctime>
 
 int score, aya_frame;
 double launch_timer;
 
-float aya_x, aya_y, aya_y_speed;
-const float aya_x_speed = 100.f, aya_y_accel = 900.f, aya_jump = -450.f;
+float aya_x, aya_y, aya_y_speed, pillar_x;
+const float aya_x_speed = 480.f, aya_y_accel = 1800.f, aya_jump = -600.f;
 
 engine::SpriteSheet *aya, *bg_objects, *onbashira;
-std::queue<float> *pillar_x, *pillar_height;
+std::vector<float> *pillar_height;
 
 
 int state = 0;
@@ -21,10 +22,18 @@ void logic_game() {
         aya_y = 528.f;
         aya_y_speed = 0.f;
         aya_frame = 0;
+        score = 0;
+
 
 
         if(engine::checkKey(engine::jump)) {
             state = 1;
+
+            pillar_x = 1000.f;
+            for(int i = 0; i < 4; i++) {
+                float height = ((double)rand() / (double)RAND_MAX) * 600.f + 60.f;
+                pillar_height->push_back(height);
+            }
         }
 
     } else if(state == 1) {
@@ -40,6 +49,14 @@ void logic_game() {
     } else if(state == 2) {
         //  playing
 
+        pillar_x -= aya_x_speed * engine::deltatime / 2;
+        if(pillar_x < aya_x - (aya_x_speed * 2)) {
+            score++;
+            float height = ((double)rand() / (double)RAND_MAX) * 540.f + 90.f;
+            pillar_height->push_back(height);
+            pillar_x += aya_x_speed;
+        }
+
         if(engine::checkKeyPressed(engine::jump)) {
             aya_y_speed = aya_jump;
         }
@@ -53,6 +70,8 @@ void logic_game() {
         //  state 3
     } else if(state == 3) {
         //  end
+        pillar_height->empty();
+        score = 0;
     }
 
 
@@ -69,7 +88,14 @@ void draw_game() {
         aya->drawSpriteCentered(aya_frame, aya_x, aya_y, 0, 116, 256);
     } else if(state == 2) {
         aya->drawSpriteCentered(aya_frame, aya_x, aya_y, (-aya_y_speed / 50), 184, 184);
+        for(int i = 0; i < 4; i++) {
+            onbashira->drawSpriteCentered(0, pillar_x + aya_x_speed * i, pillar_height->at(score + i) + 360.f, 0.f, 240.f, 540.f);
+            onbashira->drawSpriteCentered(1, pillar_x + aya_x_speed * i, pillar_height->at(score + i) - 360.f, 180.f, 240.f, 540.f);
+        }
+        onbashira->buffer();
+        onbashira->draw();
     }
+
 
     aya->buffer();
     aya->draw();
@@ -77,6 +103,7 @@ void draw_game() {
 }
 
 void load_game() {
+    srand(time(NULL));
     state = 0;
     aya_frame = 0;
     
@@ -101,6 +128,7 @@ void load_game() {
     engine::SetDrawmode(engine::DrawmodeSprite);
     engine::setDrawsize(960, 720);
 
+    pillar_height = new std::vector<float>();
 
 
     // bg_objects = new engine::SpriteSheet("./data/bg.png", 4);
